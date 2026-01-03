51c83e5e6522111e736529afd64a5abd18bc84bf
Ananya Singh <ananyasinghas0826@gmail.com>
2026-01-03 23:50:31 +0530

    ReactBsics

---
+++ 1React/L68/class0/src/App.jsx
@@ -0,0 +1,8 @@
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+const App=()=>{
+  return <div>
+    <Nav/><Footer/>
+  </div>
+}
+export default App;
---
+++ 1React/L68/class0/src/components/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer=()=>{
+    return (
+        <div>THIS IS footer</div>
+    )
+}
+export default Footer
