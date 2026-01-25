9c7a9e810824a7d72be54175e9d54f141235eac7
Ananya Singh <ananyasinghas0826@gmail.com>
2026-01-26 02:33:56 +0530

    useState

--- 1React/class5/src/App.jsx
+++ 1React/class5/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState } from "react";
 const App = () => {
-  const [user,setUser]=useState('anubhav');
-  const btnClicked=()=>{
-    setUser('hehe');
+  const [marks,setMarks]= useState([60,55,89,12,29]);
+  function graceStudent(){
+    const newMarks=marks.map(function(elem){
+      return elem<=95?elem+5:elem;
+    })
+    setMarks(newMarks);
   }
   return (
     <div>
-      <h1>{user}</h1>
-      <button onClick={btnClicked}>Change User</button>
+      {marks.map(function(elem,idx){
+        return <h1 key={idx}>Student {idx+1} = {elem} ({elem>33?'PASS':'FAIL'})</h1>
+      })}
+      <button onClick={graceStudent}>Give then grace</button>
     </div>
   )
 }
