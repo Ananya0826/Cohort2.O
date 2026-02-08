e341aecf7d2a873da4695f1691754d20b7d35a7a
Ananya Singh <ananyasinghas0826@gmail.com>
2026-02-08 23:46:35 +0530

    k

--- 1React/class11/src/pages/Men.jsx
+++ 1React/class11/src/pages/Men.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Men = () => {
   return (
     <div>
-      <h1 className='text-3xl underline font-bold absolute top-1/2 left-1/2 -translate-1/2'>Men's Page</h1>
+      <h1 className='text-5xl underline font-bold absolute top-1/2 left-1/2 -translate-1/2'>Men's Page</h1>
     </div>
   )
 }
