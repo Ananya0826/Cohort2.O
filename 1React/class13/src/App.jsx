354541eae4e8ee7f76c4b21b573714bd946c169d
Ananya Singh <ananyasinghas0826@gmail.com>
2026-02-15 23:51:03 +0530

    context API starts

---
+++ 1React/class13/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AllSections from './components/AllSections';
+
+const App = () => {
+  const courseData={
+    courseName:'Cohort2.0',
+    instructor:'Sarthak bhaiya',
+    mentor:'Anubhav',
+    duration:'6 months'
+  }
+  return (
+    <div>
+      <Navbar/>
+      <AllSections courseData={courseData}/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ 1React/class13/src/components/AllCourses.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Courses from './Courses'
+
+const AllCourses = (props) => {
+    console.log(props.courseData)
+  return (
+    <div>
+      AllCourses
+      <Courses/>
+      <Courses/>
+      <Courses/>
+      <Courses/>
+    </div>
+  )
+}
+
+export default AllCourses
