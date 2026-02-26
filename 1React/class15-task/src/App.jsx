f13cf4049b34063df52ee703aa334e0cf95db4e0
Ananya Singh <ananyasinghas0826@gmail.com>
2026-02-26 23:36:33 +0530

    done done

--- 1React/class15-task/src/App.jsx
+++ 1React/class15-task/src/App.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 const App = () => {
+  const [productData,setProductData]=useState([]);
   const getData=async()=>{
     const response=await axios.get('https://fakestoreapi.com/products')
-    console.log(response.data);
+    setProductData(response.data);
+    console.log(response.data,"hehehehehehehehehehehe");
   }
   const getFirstData=async(id)=>{
     const response=await axios.get(`https://fakestoreapi.com/products/${id}`)
@@ -15,7 +17,9 @@ const App = () => {
   },[])
   return (
     <div>
-      
+      {productData.map(function(elem){
+        return <h1>{elem.data}</h1> 
+      })}
     </div>
   )
 }
